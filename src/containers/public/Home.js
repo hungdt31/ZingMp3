fe4bcc775fefc9a88beeb5768d41f4707b4084be
Tri Hung <koikoidth12@gmail.com>
2023-07-24 09:02:36 +0700

    connect api with axios

--- src/containers/public/Home.js
+++ src/containers/public/Home.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React , { useEffect } from "react";
 import { Header } from "../../components";
+import * as apis from '../../apis'
 function Home() {
+    useEffect(()=>{
+        const fetchDataHome = async() =>{
+            const response = await apis.getHome()
+            console.log(response.data.data)
+        }
+        fetchDataHome()
+    },[])
     return ( 
         <div className="overflow-y-auto">
             <div className="h-[70px] px-[59px] flex items-center">
